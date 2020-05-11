ff22de785725aa3b6612fcca2b9518497f2ef780
Duy Pham <phamduychv94@gmail.com>
2020-05-12 03:55:23 +1000

    Update

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-"# R_intro" 
+# R_intro
+
+# How to run script:
+
+``` Rscript calculate_RMSE.R [file ground truth] [student submission folder]```
+
+#### For example:
+
+``` Rscript calculate_RMSE.R ./evaluating_gold_proteome_10005.txt ./student_submission ```
+
+# Folder structure:
+
+``` student_submission/[Student name]/Single.txt (or Multiple.txt)```
+
+#### For example:
+
+``` student_submission/Nguyen_Van_A/Single.txt```
---
+++ calculate_RMSE.R
@@ -0,0 +1,47 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+# RMSE function
+RMSE <- function(ground_truth, student_result){
+  d <- c(as.matrix(ground_truth)) - c(as.matrix(ground_truth))
+  mask <- !is.na(d)
+  n_valid <- sum(mask)
+  rmse <- sqrt(sum(d[mask]^2)/n_valid)
+  return(rmse)
+}
+
+# Load ground truth
+ground_truth <- read.table("../../Downloads//evaluating_gold_proteome_10005.txt",header=TRUE)
+# Sorting gene name
+ground_truth <- ground_truth[order(ground_truth$Gene_ID),]
+# Remove Gene_ID
+ground_truth <- ground_truth[,2:length(ground_truth)]
+# Sorting sample name
+column_names <- c(sort(names(ground_truth)))
+ground_truth <- ground_truth[,column_names]
+
+
+
+result = RMSE(ground_truth,ground_truth)
+
+list_students <- list.dirs("student_submission/")
+list_students <- list_students[2:length(list_students)]
+
+RMSE_score_df <- data.frame(Student = character(),
+                           Single_RMSE = numeric(),
+                           Multiple_RMSE = numeric())
+
+for (i in 1: length(list_students)){
+  Student <- as.character(basename(list_students[i]))
+  
+  single <- read.table(paste0(list_students[i],"/Single.txt"))
+  Single_RMSE <- RMSE(ground_truth,single)
+  
+  multiple <- read.table(paste0(list_students[i],"/Multiple.txt"))
+  Multiple_RMSE <- RMSE(ground_truth,multiple)
+  
+  RMSE_score_df <- rbind(RMSE_score_df,data.frame(Student,Single_RMSE,Multiple_RMSE))
+}
+
+write.csv(RMSE_score_df,"RMSE_results.csv",row.names = F)
+print("DONE!")
